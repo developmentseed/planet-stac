f935b116a0af7108847136a6cb20c00a0027b4c1
Oliver Roick <oliver@roick.email>
2023-01-16 11:39:08 +1100

    feat: Add clear buttons for collections and date-range select

--- src/components/layout/Filter/index.jsx
+++ src/components/layout/Filter/index.jsx
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useCallback } from "react";
 import T from "prop-types";
 import {
   Button,
+  IconButton,
   Stack,
   Text,
   Divider,
+  Box,
 } from "@chakra-ui/react";
+import { DeleteIcon } from "@chakra-ui/icons";
 
 import { DateSelect } from "./DateSelect";
 import { CollectionSelect } from "./CollectionSelect";
@@ -16,23 +19,57 @@ function Filter({ collections: collectionProps, dateRange: dateRangeProps }) {
   const { collections, selectedCollections, setCollections } = collectionProps;
   const { dateRangeFrom, setDateRangeFrom, dateRangeTo, setDateRangeTo } = dateRangeProps;
 
+  const handleClearCollections = useCallback(() => setCollections([]), [setCollections]);
+  const handleClearDateRange = useCallback(() => {
+    setDateRangeFrom("");
+    setDateRangeTo("");
+  }, [setDateRangeFrom, setDateRangeTo]);
+
   return (
-    <Stack direction="row" alignItems="center" gap="3" h="30px">
+    <Stack direction="row" alignItems="center" h="30px" gap="3">
       <Text fontWeight="bold">Filter:</Text>
-      <CollectionSelect
-        collections={collections}
-        selectedCollections={selectedCollections}
-        setCollections={setCollections}
-      />
+      <Box>
+        <CollectionSelect
+          collections={collections}
+          selectedCollections={selectedCollections}
+          setCollections={setCollections}
+        />
+        {selectedCollections.length > 0 && (
+          <IconButton
+            w="3"
+            minWidth="3"
+            ml="3"
+            aria-label="Clear collection selection"
+            variant="link"
+            icon={<DeleteIcon boxSize={3} />}
+            onClick={handleClearCollections}
+          />
+        )}
+      </Box>
       <Divider orientation="vertical" />
-      <DateSelect
-        dateRangeFrom={dateRangeFrom}
-        setDateRangeFrom={setDateRangeFrom}
-        dateRangeTo={dateRangeTo}
-        setDateRangeTo={setDateRangeTo}
-      />
+      <Box>
+        <DateSelect
+          dateRangeFrom={dateRangeFrom}
+          setDateRangeFrom={setDateRangeFrom}
+          dateRangeTo={dateRangeTo}
+          setDateRangeTo={setDateRangeTo}
+        />
+        {(dateRangeFrom || dateRangeTo) && (
+          <IconButton
+            w="3"
+            minWidth="3"
+            ml="3"
+            aria-label="Clear date-range selection"
+            variant="link"
+            icon={<DeleteIcon boxSize={3} />}
+            onClick={handleClearDateRange}
+          />
+        )}
+      </Box>
       <Divider orientation="vertical" />
-      <Button variant="link">Select area</Button>
+      <Box>
+        <Button variant="link">Select area</Button>
+      </Box>
     </Stack>
   );
 }
