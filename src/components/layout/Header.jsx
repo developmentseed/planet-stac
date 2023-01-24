7739eaa780d38e9156c8fb221ca82239272fa502
Oliver Roick <oliver@roick.email>
2023-01-24 13:34:55 +1100

    feat: Add switching STAC APIs

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -10,25 +10,66 @@ import {
   AlertDialogOverlay,
   FormControl,
   FormLabel,
+  FormErrorMessage,
   Input
 } from "@chakra-ui/react";
+import { useEffect } from "react";
 
-function Header({ title, stacApiUrl, setStacApiUrl }) {
-  const [ url, setUrl ] = useState(stacApiUrl);
+function isStacSearchApi(meta) {
+  const { stac_version, links } = meta;
+  if (!stac_version) {
+    return "This API is not a STAC catalog.";
+  }
+
+  const search = links.find(({ rel }) => rel === "search");
+  if (!search) {
+    return "This STAC catalog does not support STAC search.";
+  }
+}
+
+function Header({ stacApiUrl, setStacApiUrl }) {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const cancelRef = useRef();
+
+  const [ title, setTitle ] = useState();
+  const [ url, setUrl ] = useState(stacApiUrl);
+  const [ urlError, setUrlError ] = useState();
 
   const handleUrlChange = useCallback((e) => setUrl(e.target.value), [setUrl]);
 
   const handleSubmit = useCallback(() => {
-    setStacApiUrl(url);
-    onClose();
+    if (!url) {
+      setUrlError("Enter a URL to a STAC catalog.");
+    } else {
+      fetch(url)
+        .then(r => r.json())
+        .then(r => {
+          const error = isStacSearchApi(r);
+          if (error) {
+            setUrlError(error);
+          } else {
+            setTitle(r.title || r.id);
+            setStacApiUrl(url);
+            onClose();
+          }
+        })
+        .catch(() => setUrlError("Unable to open this STAC catalog"));
+    }
   }, [onClose, setStacApiUrl, url]);
 
   const handleCancel = useCallback(() => {
     setUrl(stacApiUrl);
     onClose();
   }, [onClose, setUrl, stacApiUrl]);
+
+  useEffect(() => {
+    // Set the title when the app is initialised.
+    if (!title) {
+      fetch(url)
+        .then(r => r.json())
+        .then(({ id, title }) => setTitle(title || id));
+    }
+  }, [title, url]);
 
   return (
     <>
@@ -50,9 +91,12 @@ function Header({ title, stacApiUrl, setStacApiUrl }) {
 
             <AlertDialogBody>
               <form>
-                <FormControl isRequired>
+                <FormControl isRequired isInvalid={urlError}>
                   <FormLabel>Enter STAC-API URL</FormLabel>
                   <Input value={url} onChange={handleUrlChange} />
+                  {urlError && (
+                    <FormErrorMessage>{urlError}</FormErrorMessage>
+                  )}
                 </FormControl>
               </form>
             </AlertDialogBody>
@@ -73,7 +117,6 @@ function Header({ title, stacApiUrl, setStacApiUrl }) {
 }
 
 Header.propTypes = {
-  title: T.string.isRequired,
   stacApiUrl: T.string,
   setStacApiUrl: T.func.isRequired,
 };
